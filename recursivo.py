e7212d16b42b806140330567f03ed91248c60b9b
epilihP <philipegcustodio@gmail.com>
2025-02-27 21:36:16 -0300

    first

---
+++ recursivo.py
@@ -0,0 +1,21 @@
+# EX 1 FATORIAL
+def fatorial_recursivo(fatorial):
+
+    if fatorial == 0 or fatorial ==1:
+        return 1
+
+    return fatorial  * fatorial_recursivo(fatorial -1)
+    
+
+# EX 2 SOMA
+
+def soma_list_recursivasso(lista):
+    if not lista == list:
+        return 0
+    
+    return 0 + soma_list_recursivasso[1:]
+
+#  EX 3 INVERSÃO DE STRING
+
+
+
